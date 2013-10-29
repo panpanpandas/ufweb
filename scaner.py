00e65b00e111164102ca72a7dc703ae537be9ed0
pan <panpandas@gmail.com>
2013-10-28 21:52:52 -0700

    fix scanner with recent performance change on sqlDAM; add violible check in scanner; replace trade list with violible stocks, 1.36 sharpe ratio from 2009 to 2013, -0.19 sharpe ration from 2007 to 2009

--- scaner.py
+++ scaner.py
@@ -4,27 +4,38 @@ Created on Nov 6, 2011
 @author: ppa
 '''
 from ultrafinance.dam.DAMFactory import DAMFactory
-from ultrafinance.pyTaLib.indicator import mean
+from ultrafinance.pyTaLib.indicator import mean, stddev
 
 symbols = []
-with open("/Users/ppa/workspace/ufweb/conf/crawler.spy_nasdaq.list", 'r') as f:
+with open("/Users/ppa/workspace/ufweb/conf/crawler.dev.list", 'r') as f:
     for line in f.readlines():
         symbols.append(line.strip())
 
 dam = DAMFactory.createDAM("sql", {'db': 'sqlite:////data/stock.sqlite'})
-ticks = dam.readBatchTupleQuotes(symbols, 20130710, None)
+dateTicks = dam.readBatchTupleQuotes(symbols, 20121010, None)
 
+
+symbolTicks = {}
+for timeStamp in sorted(dateTicks.iterkeys()):
+    for symbol, tick in dateTicks[timeStamp].iteritems():
+        if symbol not in symbolTicks:
+            symbolTicks[symbol] = []
+
+        symbolTicks[symbol].append(tick)
 
 d = {}
-for symbol, ticks in ticks.items():
+for symbol, ticks in symbolTicks.iteritems():
     if symbol in d:
         continue
 
-    ticks = ticks[-60:]
-    avgVolumn = mean([tick.volume for tick in ticks])
     avgClose = mean([tick.close for tick in ticks])
+    std = 100 * stddev([tick.close for tick in ticks])/avgClose
+    print "std for %s is %f" % (symbol, std)
 
-    if avgVolumn > 50000 and avgClose > 8 and avgClose < 50:
+    ticks = ticks[-30:]
+    avgVolumnDollar = mean([tick.volume * tick.close for tick in ticks])
+
+    if avgVolumnDollar > 1000000 and avgClose > 8 and avgClose < 50 and std > 10:
         d[symbol] = "good"
     else:
         d[symbol] = "bad"
